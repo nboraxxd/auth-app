4211971e976c6d16bc28b6922d882b45108433be
bora <nhatbaoxxd@gmail.com>
2023-12-07 21:02:40 +0700

    Connect to the database

--- server/src/constants/config.js
+++ server/src/constants/config.js
@@ -5,7 +5,5 @@ config()
 export const envConfig = {
   port: process.env.PORT || 3001,
   host: process.env.HOST,
-  dbName: process.env.DB_NAME,
-  dbUsername: process.env.DB_USERNAME,
-  dbPassword: process.env.DB_PASSWORD,
+  mongoUri: process.env.MONGO_URI,
 }
--- server/src/index.js
+++ server/src/index.js
@@ -1,7 +1,19 @@
 import express from 'express'
 import { envConfig } from '@/constants/config'
+import mongoose from 'mongoose'
 
 const PORT = envConfig.port
+const MONGO_URI = envConfig.mongoUri
+
+mongoose
+  .connect(MONGO_URI)
+  .then(() => {
+    console.log('🎉 Connected to database')
+  })
+  .catch((err) => {
+    console.log('🚧 Error connecting to database', err)
+  })
+
 const app = express()
 
 app.listen(PORT, () => {
